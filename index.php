7c931a12efd3b74861f8b1fdc896144127e93683
Ruslan <puhov.r6@gmail.com>
2025-03-05 23:44:06 +0500

    Add class User

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ require_once 'Validate.php';
 require_once 'Input.php';
 require_once 'Token.php';
 require_once 'Session.php';
+require_once 'User.php';
 
 //$users = Database::getInstance()->query("SELECT * FROM users WHERE username IN (?, ?)", ['John Doe','Jane Koe']);
 //$users = Database::getInstance()->get('users', ['password', '=', 'password1']);
@@ -49,8 +50,14 @@ if (Input::exists()) {
 
 
         if ($validation->passed()) {
+
+            $user = new User();
+            $user->create([
+                    'username' => Input::get('username'),
+                    'password' => password_hash(Input::get('password'), PASSWORD_DEFAULT),
+            ]);
+
             Session::flash('success','Регистрация успешна!');
-//            header('Location: /test.php');
         } else {
             foreach ($validation->errors() as $error) {
                 echo $error . "</br>";
